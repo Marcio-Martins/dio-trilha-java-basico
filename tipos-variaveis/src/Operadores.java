b8a249ab718d5deeebb13eb907512755a001af06
Márcio Martins <164286268+Marcio-Martins@users.noreply.github.com>
2024-08-31 23:16:45 -0300

    novo commit

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+     
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        //TRUE OU FALSE
+        //String resultado= a==b ? "verdadeiro" : "falso";
+
+        int resultado= a==b ? 1 : 0;
+        //if(a==b)
+            //resultado = "true";
+        //else
+            //resultado = "false";
+
+
+        System.out.println(resultado);
+
+    }
+}
